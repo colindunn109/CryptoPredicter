29056039279fb432248175b14a698e644a9aedd9
Colin <cjdunn@ucsc.edu>
2018-06-21 12:37:23 -0700

    adding reddit

--- cryptopredicter.py
+++ cryptopredicter.py
@@ -1,26 +1,55 @@
+#! usr/bin/env python3
+
 '''
 Utilizes sentiment and opinion analysis to guess how future coins will change
 Takes into account positive/negative opinions, and also people's bias
 Analyses your own twitter, must follow crypto news to be helpful
+
+High level Idea:
+	1. Scrape Reddit for N/E/B info, and use sentiment analysis
+	2. Scrape Twitter ^^^^
+	3. Combine the results of these and use some math to get an overall score
+	4. After this, we'll use past trends and estimation lines to see how these match up 
+	5. Maybe train a neural net to see how accurate score and estimation lines were
+	   with past trends that already exist
 '''
 
 import tweepy
+import praw
 from textblob import TextBlob
 
-myKey = 'rtMn5HeH9fvbsFjBVv7bqoOOA'
-mySecret = 'nkm56LU8FEwRZdaoM3rIcuY252fx74TaG5CYXSpP2bkUpiuwzm'
-accessToken = '923079179549097985-r1bdWg8OYvyHaLCHPkN6K16Xpx3YRJv'
-tokenSecret = 'LgUaydtZqgTus5XcTyuCghDgaMmO6XQ0OCfuAQFnlw7pK'
+
+#-----------Twitter Init--------------
+myKey = '***'
+mySecret = '***'
+accessToken = '***'
+tokenSecret = '***'
 
 auth = tweepy.OAuthHandler(myKey, mySecret)
 auth.set_access_token(accessToken, tokenSecret)
-
 api = tweepy.API(auth)
 
 neoTweets = api.search('Neo')
 ethTweets = api.search('Ethereum')
 bitTweets = api.search('Bitcoin')
 neoScore, ethScore, bitScore = 0, 0, 0
+
+#------------Reddit Init---------------
+reddit = praw.Reddit(client_id='***', 
+					 client_secret='***',
+					 password='***', 
+					 user_agent='***', 
+					 username='***' 
+					 )
+neoSub = reddit.subreddit('Neo')
+topNeo = neoSub.hot(limit=50)
+for post in topNeo:
+	tAnalysis = TextBlob(post.title)
+	dAnalysis = TextBlob(post.selftext)
+	tPolarity = tAnalysis.sentiment.polarity
+	tSubject = tAnalysis.sentiment.subjectivity
+	dPolarity = dAnalysis.sentiment.polarity
+	print(tPolarity,dPolarity)
 
 for tweet in neoTweets:
 	analysis = TextBlob(tweet.text)
